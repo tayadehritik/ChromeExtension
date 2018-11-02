62c18e97f9ead453c8cccd9053e124c988e56328
Hritik Tayade <braceritchie@protonmail.com>
2018-11-02 13:57:04 +0530

    server added

---
+++ ww.py
@@ -0,0 +1,35 @@
+import asyncio
+import pathlib
+import ssl
+import websockets
+import re
+async def hello(websocket, path):
+    url = ""
+    while True:
+        name = await websocket.recv()
+ 
+        if(f"{name}"[0] == "p"):
+            url = f"{name}"[1:]
+            await websocket.send("pong")
+        else:
+            
+            exp = re.compile("(.{0,})\[\](.{0,})")
+            match = exp.match(f"{name}"+" [] "+url)
+            ulocater = match.group(2)
+            tag = match.group(1)
+            f = open('chlog.txt', 'ab+')
+            buff = tag + ulocater +"\n"
+            f.write(buff.encode())
+            f.close()
+               
+            print(tag,ulocater)
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain(
+    pathlib.Path(__file__).with_name('latfullchain.pem'))
+
+start_server = websockets.serve(
+    hello, '0.0.0.0', 8765, ssl=ssl_context)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
